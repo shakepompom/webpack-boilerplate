bbe854c1857141aa4fda3dc7bae53af2db7aa8d0
cu <u.chris1608@gmail.com>
2018-10-14 22:52:49 +0300

    Added eslint-config-airbnb-base

--- assets/js/modules/svg-store/svg-store.js
+++ assets/js/modules/svg-store/svg-store.js
@@ -1,5 +1,6 @@
 import svgxhr from 'webpack-svgstore-plugin/src/helpers/svgxhr';
 
+// eslint-disable-next-line no-underscore-dangle
 const __svg__ = {
     path: '../../../icons/**/*.svg',
     name: './static/svg/[hash].sprite.svg',
